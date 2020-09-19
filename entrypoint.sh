05198ac76dbe88766e331d2396134e5868154234
Aleksei Ivanov <contact@aleksei.dev>
2020-09-19 18:00:54 +0300

    Refactor out yq

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,40 +14,7 @@ if [[ -z "$GID" ]]; then
     GID=$(id -g)
 fi
 
-yq() {
-    docker run --rm -i -v "${PWD}":/workdir mikefarah/yq yq "$@"
-}
-
-read_value() {
-    if [[ -z "$3" ]]; then
-        FILE="$BLUEPRINT_FILE_FINAL"
-    else
-        FILE="$3"
-    fi
-
-    printf -v "$1" "$(yq read "$FILE" "$2" 2>/dev/null)"
-}
-
-read_array() {
-    if [[ -z "$3" ]]; then
-        FILE="$BLUEPRINT_FILE_FINAL"
-    else
-        FILE="$3"
-    fi
-    
-    readarray -t "$1" < <(yq read "$FILE" "$2[*]")
-}
-
-read_keys() {
-    if [[ -z "$3" ]]; then
-        FILE="$BLUEPRINT_FILE_FINAL"
-    else
-        FILE="$3"
-    fi
-    
-    readarray -t "$1" < <(yq read "$FILE" "$2" --tojson | \
-                        jq -r '. | keys[]')
-}
+source ./includes/yq.sh
 
 init_default_service() {
     DEFAULT_SERVICE=$(cat $DIR/default_service 2>/dev/null)
