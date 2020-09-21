3b9b4da505cec2c845e96f0d4189b8ecbe60d7d7
Aleksei Ivanov <contact@aleksei.dev>
2020-09-21 15:38:31 +0300

    Fix version command

--- commands/version.sh
+++ commands/version.sh
@@ -14,5 +14,11 @@ if  ! $AS_FUNCTION; then
     printf "Current version: "
 fi
 
+PREVIOUS_DIR=$PWD
+
+cd $ROOT_DIR
+
 printf $(git describe --match "v*" --abbrev=0 --tags)
 printf "\n"
+
+cd $PREVIOUS_DIR
