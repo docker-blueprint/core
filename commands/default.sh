953247dfd23e21718e91ad8df8801643ffa8fb47
Aleksei Ivanov <contact@aleksei.dev>
2020-12-05 23:24:50 +0300

    Update help

--- commands/default.sh
+++ commands/default.sh
@@ -9,7 +9,7 @@ shift
 case $1 in
     -h|--help)
         printf "${CMD_COL}default clear${RESET}\t\t\tClear default service\n"
-        printf "${CMD_COL}default${RESET} ${ARG_COL}<service>${RESET}\t\tSet default service to run commands against (usually set by preset)\n"
+        printf "${CMD_COL}default${RESET} ${ARG_COL}<service>${RESET}\t\tSet default service to run commands against (usually set by the blueprint)\n"
         exit
 
         ;;
