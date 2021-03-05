bc1ac5b6bacfbe04634d8d75ad0586beb137a92a
Aleksei Ivanov <contact@aleksei.dev>
2021-03-05 07:27:02 +0300

    Fix variable substitution

--- includes/preprocessor/substitute-vars.sh
+++ includes/preprocessor/substitute-vars.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-VARIABLES=($(env | awk -F= '{ print $1 }' | grep '^BLUEPRINT_'))
+PREFIX="BLUEPRINT_"
+
+VARIABLES=($(env | awk -F= '{ print $1 }' | grep "^$PREFIX"))
 
 BUFFER="$1"
 
@@ -8,9 +10,9 @@ for variable in "${VARIABLES[@]}"; do
     # Escape variable value to use with sed
     value="$(echo ${!variable} | sed -E 's|[][\\/.*^$]|\\&|g')"
     # Uncomment the line where the variable is used
-    BUFFER="$(echo "$BUFFER" | sed -E "s/#\s*(.*%$variable)/\1/g")"
+    BUFFER="$(echo "$BUFFER" | sed -E "s/#\s*(.*%${variable#"$PREFIX"})/\1/g")"
     # Substitute the variable with its value
-    BUFFER="$(echo "$BUFFER" | sed -E "s/%$variable/$value/g")"
+    BUFFER="$(echo "$BUFFER" | sed -E "s/%${variable#"$PREFIX"}/$value/g")"
 done
 
 printf "%s" "$BUFFER"
