904abaf72e60433776e3975bc70957af1e8b8ab9
Aleksei Ivanov <contact@aleksei.dev>
2021-03-06 06:40:52 +0300

    Update init-compose.sh

--- includes/entrypoint/init-compose.sh
+++ includes/entrypoint/init-compose.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-export DOCKER_COMPOSE="docker-compose -f docker-compose.yml"
+ENV_PREFIX=()
+
+ENV_PREFIX+=("COMPOSE_PROJECT_NAME=${PROJECT_NAME}")
+
+FILES=()
+
+FILES+=("-f docker-compose.yml")
+
+export DOCKER_COMPOSE="env ${ENV_PREFIX[@]} docker-compose ${FILES[@]}"
