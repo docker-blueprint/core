1c4e22ae7deca7e43522eb71e4b4c3c2c0fc610a
Aleksei Ivanov <contact@aleksei.dev>
2021-03-17 08:49:12 +0300

    Update parse-directive.sh

--- includes/preprocessor/parse-directive.sh
+++ includes/preprocessor/parse-directive.sh
@@ -6,16 +6,18 @@ substitute_vars() {
 
 # Line starts with a comment symbol with a non-whitespace
 # symbol following right after it, so it can potentially be a directive.
-if [[ "$(echo "$1" | cut -c1)" = "#" && "$(echo "$1" | cut -c2)" != " " ]]; then
+if [[ "$(echo "$1" | cut -c1)" = "#" ]]; then
     # Strip comment symbol from the beginning of the string
     INPUT="$(echo "$1" | cut -c2-)"
+    IFS_OLD="$IFS"
     IFS=' ' read -r -a items <<<$INPUT
+    IFS="$IFS_OLD"
 
     if [[ "${items[0]}" = "include" ]]; then
         resource_path="$(substitute_vars "${items[1]}")"
         file="$(AS_FUNCTION=true bash $ENTRYPOINT process "$resource_path")"
 
-        if [[ -f $file ]]; then
+        if [[ -f "$file" ]]; then
             printf "%s" "$(cat $file)"
             rm -f "$file" # Clean up after processing
             exit # The given path was read successfully
