1f4ba0d28f676d7014a9db7a370d46394bb61460
Aleksei Ivanov <contact@aleksei.dev>
2021-03-19 01:56:32 +0300

    Update uninstaller

--- uninstall.sh
+++ uninstall.sh
@@ -2,7 +2,7 @@
 
 SUDO="$(which sudo 2>/dev/null)"
 PROJECT_DIR=~/.docker-blueprint
-EXEC_PATH=$(which docker-blueprint 2>/dev/null)
+EXEC_PATH="$(which docker-blueprint 2>/dev/null)"
 
 if [[ -n $EXEC_PATH ]]; then
     printf "Removing link..."
@@ -10,7 +10,7 @@ if [[ -n $EXEC_PATH ]]; then
     printf " done\n"
 fi
 
-EXEC_PATH=$(which dob 2>/dev/null)
+EXEC_PATH="$(which dob 2>/dev/null)"
 
 if [[ -n $EXEC_PATH ]]; then
     printf "Removing link..."
