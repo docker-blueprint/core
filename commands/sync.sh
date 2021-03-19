e2cf8ad61553cdcd66faea811b2fc67f12dd61d2
Aleksei Ivanov <contact@aleksei.dev>
2021-03-19 04:17:51 +0300

    Format sync.sh

--- commands/sync.sh
+++ commands/sync.sh
@@ -14,21 +14,21 @@ MODE_NO_CHOWN=false
 
 while [[ "$#" -gt 0 ]]; do
     case $1 in
-        -h|--help)
-            printf "${CMD_COL}sync${RESET}"
-            printf "\t\t\t\t"
-            printf "Synchronize development environment container\n"
+    -h | --help)
+        printf "${CMD_COL}sync${RESET}"
+        printf "\t\t\t\t"
+        printf "Synchronize development environment container\n"
 
-            printf "  ${FLG_COL}--no-chown${RESET}"
-            printf "\t\t\tDo not attempt to chown home directory (i.e. when there are a lot of local files)\n"
+        printf "  ${FLG_COL}--no-chown${RESET}"
+        printf "\t\t\tDo not attempt to chown home directory (i.e. when there are a lot of local files)\n"
 
-            exit
+        exit
 
-            ;;
-        --no-chown)
-            MODE_NO_CHOWN=true
+        ;;
+    --no-chown)
+        MODE_NO_CHOWN=true
 
-            ;;
+        ;;
     esac
 
     shift
@@ -48,9 +48,9 @@ if [[ -n "$SYNC_USER" ]]; then
     echo "Setting UID to $UID..."
     $DOCKER_COMPOSE exec "$DEFAULT_SERVICE" usermod -u "$UID" "$SYNC_USER"
 
-    if ! $MODE_NO_CHOWN;  then
+    if ! $MODE_NO_CHOWN; then
         HOME_DIR="$($DOCKER_COMPOSE exec --user="$SYNC_USER" "$DEFAULT_SERVICE" env | grep '^HOME=' | sed -r 's/^HOME=(.*)/\1/' | sed 's/\r//' | sed 's/\n//')"
-        if [[ -n "$HOME_DIR" ]];  then
+        if [[ -n "$HOME_DIR" ]]; then
             echo "Recursively chowning home directory '$HOME_DIR'..."
             $DOCKER_COMPOSE exec "$DEFAULT_SERVICE" chown -R "$SYNC_USER" "$HOME_DIR"
         else
@@ -95,7 +95,7 @@ fi
 # Restart container to apply chown
 #
 
-if ! $MODE_NO_CHOWN;  then
+if ! $MODE_NO_CHOWN; then
     echo "Restarting container '$DEFAULT_SERVICE'..."
     $DOCKER_COMPOSE restart "$DEFAULT_SERVICE"
 fi
