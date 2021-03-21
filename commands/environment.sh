76230c464830cac40b3ccb1d0f214b00c5876914
Aleksei Ivanov <contact@aleksei.dev>
2021-03-21 06:46:35 +0300

    Add environment command

---
+++ commands/environment.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+shift
+
+MODE_FORCE=false
+MODE_QUIET=false
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+    -h | --help)
+        printf "${CMD_COL}environment clear${RESET}\t\tRemove current specified environemnt\n"
+        printf "${CMD_COL}environment${RESET} ${ARG_COL}<name>${RESET}"
+        printf "\t\tSet blueprint environment\n"
+        exit
+
+        ;;
+    -f | --force)
+        MODE_FORCE=true
+
+        ;;
+    -q | --quiet)
+        MODE_QUIET=true
+
+        ;;
+    *)
+        ENVIRONMENT=$1
+        ;;
+    esac
+
+    shift
+done
+
+if [[ "$ENVIRONMENT" != "clear" ]]; then
+    yq_read_value BLUEPRINT "from"
+    BLUEPRINT_DIR=$(AS_FUNCTION=true bash $ENTRYPOINT pull $BLUEPRINT)
+    BLUEPRINT_QUALIFIED_NAME=$(AS_FUNCTION=true bash $ENTRYPOINT pull $BLUEPRINT --get-qualified-name)
+
+    if [[ $? -ne 0 ]]; then
+        printf "${RED}ERROR${RESET}: Unable to pull blueprint '$BLUEPRINT'.\n"
+        exit 1
+    fi
+
+    if [[ -z "$ENVIRONMENT" ]]; then
+        if [[ -d "$BLUEPRINT_DIR/env" ]]; then
+            printf "${GREEN}%s${RESET}:\n" "Available environments"
+
+            for environment in $BLUEPRINT_DIR/env/*; do
+                printf -- "- %s\n" "${environment#"$BLUEPRINT_DIR/env/"}"
+            done
+        else
+            printf "The blueprint '${YELLOW}$BLUEPRINT_QUALIFIED_NAME${RESET}' has no environments.\n"
+        fi
+
+        exit 1
+    fi
+
+    if [[ ! -d "$BLUEPRINT_DIR/env/$ENVIRONMENT" ]]; then
+        printf "${RED}ERROR${RESET}: No such environment '$ENVIRONMENT'.\n"
+        exit 1
+    fi
+
+    yq_write_value "environment" "$ENVIRONMENT"
+else
+    yq eval "del(.environment)" -i "$PROJECT_BLUEPRINT_FILE"
+fi
+
+if ! $MODE_QUIET; then
+    if ! $MODE_FORCE; then
+        printf "Do you want to rebuild the project? (run with ${FLG_COL}--force${RESET} to always build)\n"
+        printf "${YELLOW}WARNING${RESET}: This will ${RED}overwrite${RESET} existing docker files [y/N] "
+        read -n 1 -r
+        if [[ $REPLY =~ ^[Yy]$ ]]; then
+            echo ""
+            bash $ENTRYPOINT build --force
+        fi
+    else
+        bash $ENTRYPOINT build --force
+    fi
+fi
