0b15ed7e9eacb62b507c9038c36a36587d77e49c
Aleksei Ivanov <contact@aleksei.dev>
2021-04-18 17:50:59 +0300

    Refactor script variables initialization

--- commands/build.sh
+++ commands/build.sh
@@ -104,76 +104,10 @@ non_debug_print " ${GREEN}done${RESET}\n"
 # Read generated configuration
 #
 
-debug_newline_print "Reading configuration..."
-
-# Read values from merged blueprint
-
-yq_read_value DEFAULT_SERVICE "default_service" "$BLUEPRINT_PATH" && non_debug_print "."
-yq_read_keys BUILD_ARGS_KEYS "build_args" "$BLUEPRINT_PATH" && non_debug_print "."
-
-echo "$DEFAULT_SERVICE" >"$LOCAL_DIR/default_service"
-
-BUILD_ARGS=()
-SCRIPT_VARS=()
-
-add_variable() {
-    debug_print "Added variable $1='$2'"
-    BUILD_ARGS+=("--build-arg $1='$2'")
-    SCRIPT_VARS+=("BLUEPRINT_$1=$2")
-}
-
-add_variable "BLUEPRINT_DIR" "${BLUEPRINT_DIR#"$PWD/"}"
-add_variable "ENV_DIR" "${ENV_DIR#"$PWD/"}"
-add_variable "ENV_NAME" "$ENV_NAME"
-
-for variable in ${BUILD_ARGS_KEYS[@]}; do
-    yq_read_value value "build_args.$variable" "$BLUEPRINT_PATH" && non_debug_print "."
-
-    # Replace build argument value with env variable value if it is set
-    if [[ -n ${!variable+x} ]]; then
-        value="${!variable:-}"
-    fi
-
-    add_variable "$variable" "$value" && non_debug_print "."
-done
-
-yq_read_keys DEPENDENCIES_KEYS "dependencies" "$BLUEPRINT_PATH" && non_debug_print "."
-
-for key in "${DEPENDENCIES_KEYS[@]}"; do
-    yq_read_array DEPS "dependencies.$key" "$BLUEPRINT_PATH" && non_debug_print "."
-    key="$(echo "$key" | tr [:lower:] [:upper:])"
-    add_variable "DEPS_$key" "${DEPS[*]}"
-done
-
-yq_read_keys PURGE_KEYS "purge" "$BLUEPRINT_PATH" && non_debug_print "."
-
-for key in "${PURGE_KEYS[@]}"; do
-    yq_read_array PURGE "purge.$key" "$BLUEPRINT_PATH" && non_debug_print "."
-    key="$(echo "$key" | tr [:lower:] [:upper:])"
-    add_variable "PURGE_$key" "${PURGE[*]}"
-done
-
-env_name="$(echo "$ENV_NAME" | tr [:lower:] [:upper:] | tr - _)"
-
-if [[ -n "$ENV_NAME" ]]; then
-    add_variable "ENV_${env_name}_DIR" "$ENV_DIR"
-fi
-
-for module in "${MODULES_TO_LOAD[@]}"; do
-    module_name="$(echo "$module" | tr [:lower:] [:upper:] | tr "/-" _)"
-
-    path="${BLUEPRINT_DIR#"$PWD/"}/modules/$module"
-    if [[ -d "$path" ]]; then
-        add_variable "MODULE_${module_name}_DIR" "$path"
-    fi
-
-    path="${ENV_DIR#"$PWD/"}/modules/$module"
-    if [[ -d "$path" ]]; then
-        add_variable "ENV_${env_name}_MODULE_${module_name}_DIR" "$path"
-    fi
-done
-
-non_debug_print " ${GREEN}done${RESET}\n"
+# export SCRIPT_VARS
+# export SCRIPT_VARS_ENV
+# export SCRIPT_VARS_BUILD_ARGS
+source "$ROOT_DIR/includes/get-script-vars.sh"
 
 #
 # Build docker-compose.yml
@@ -362,10 +296,10 @@ done
 #
 
 if $MODE_NO_CACHE; then
-    BUILD_ARGS+=("--no-cache")
+    SCRIPT_VARS_BUILD_ARGS+=("--no-cache")
 fi
 
-command="$DOCKER_COMPOSE build ${BUILD_ARGS[@]}"
+command="$DOCKER_COMPOSE build ${SCRIPT_VARS_BUILD_ARGS[@]}"
 
 debug_print "Running command: $command"
 
