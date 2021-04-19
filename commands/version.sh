f07db756cc7348938caa1a96919524790c0b7a5a
Aleksei Ivanov <contact@aleksei.dev>
2021-04-19 17:28:57 +0300

    Update version.sh
    Add ability to switch version

--- commands/version.sh
+++ commands/version.sh
@@ -4,19 +4,35 @@ shift
 
 case $1 in
 -h | --help)
-    printf "${CMD_COL}version${RESET}\t\t\tGet current version\n"
+    printf "${CMD_COL}version${RESET} [change ${ARG_COL}<pathspec>${RESET}]"
+    printf "\tGet current version or switch version to <pathspec>\n"
     exit
+    ;;
+change)
+    NEW_VERSION="$2"
 
+    if [[ -z "$NEW_VERSION" ]]; then
+        printf "${RED}ERROR${RESET}: pathspec must not be empty\n"
+        bash $ENTRYPOINT version -h
+        exit 1
+    fi
     ;;
 esac
-
-if ! $AS_FUNCTION; then
-    printf "Current version: "
-fi
 
 PREVIOUS_DIR=$PWD
 
 cd $ROOT_DIR
+
+if [[ -n "$NEW_VERSION" ]]; then
+    git checkout "$NEW_VERSION"
+    if [[ $? > 0 ]]; then
+        exit 1
+    fi
+fi
+
+if ! $AS_FUNCTION; then
+    printf "Current version: "
+fi
 
 printf "$(git describe --match "v*" --abbrev=0 --tags)"
 printf " ($(git rev-parse --short HEAD))"
