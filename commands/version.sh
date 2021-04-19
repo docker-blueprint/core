133bc013042b0be67650f56d40fb055abbc1b19c
Aleksei Ivanov <contact@aleksei.dev>
2021-04-19 17:40:11 +0300

    Update process.sh
    Update help text

--- commands/version.sh
+++ commands/version.sh
@@ -4,35 +4,19 @@ shift
 
 case $1 in
 -h | --help)
-    printf "${CMD_COL}version${RESET} [change ${ARG_COL}<pathspec>${RESET}]"
-    printf "\tGet current version or switch version to <pathspec>\n"
+    printf "${CMD_COL}version${RESET}\t\t\tGet current version\n"
     exit
-    ;;
-change)
-    NEW_VERSION="$2"
 
-    if [[ -z "$NEW_VERSION" ]]; then
-        printf "${RED}ERROR${RESET}: pathspec must not be empty\n"
-        bash $ENTRYPOINT version -h
-        exit 1
-    fi
     ;;
 esac
-
-PREVIOUS_DIR=$PWD
-
-cd $ROOT_DIR
-
-if [[ -n "$NEW_VERSION" ]]; then
-    git checkout "$NEW_VERSION"
-    if [[ $? > 0 ]]; then
-        exit 1
-    fi
-fi
 
 if ! $AS_FUNCTION; then
     printf "Current version: "
 fi
+
+PREVIOUS_DIR=$PWD
+
+cd $ROOT_DIR
 
 printf "$(git describe --match "v*" --abbrev=0 --tags)"
 printf " ($(git rev-parse --short HEAD))"
