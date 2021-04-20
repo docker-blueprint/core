fe92c49e04d55b01b20396b6343840f4af35199b
Aleksei Ivanov <contact@aleksei.dev>
2021-04-20 21:11:59 +0300

    Refactor out yq install script

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+HIGHLIGHT="\033[1;33m"
+RESET="\033[0;0m"
+
 REQUIREMENTS=(
     git
     docker
@@ -13,18 +16,7 @@ for PROGRAM in "${REQUIREMENTS[@]}"; do
     fi
 done
 
-if [[ -z "$(which yq)" ]]; then
-    printf "You do not appear to have 'yq' installed (https://github.com/mikefarah/yq)\n"
-    printf "For the best experience it is recommended to install a standalone version of 'yq'\n"
-    printf "\n"
-    printf "Do you want to attempt to automatically install 'yq'? [Y/n] "
-    read -n 1 -r
-    echo ""
-    if [[ -z "$REPLY" ]] || [[ $REPLY =~ ^[Yy]$ ]]; then
-        echo "Trying to install using webi..."
-        curl -sS https://webinstall.dev/yq@4 | bash
-    fi
-fi
+source "$(dirname "$BASH_SOURCE")/installer/yq.sh"
 
 PROJECT_DIR=~/.docker-blueprint
 ENTRYPOINT="$PROJECT_DIR/entrypoint.sh"
