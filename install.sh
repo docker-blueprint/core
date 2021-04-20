3fa1dedfcc3f6570a2445d02f7397c791518d1a0
Aleksei Ivanov <contact@aleksei.dev>
2021-04-20 21:28:33 +0300

    Embed install scripts into install.sh

--- install.sh
+++ install.sh
@@ -15,8 +15,65 @@ if grep -qEi "(Microsoft|WSL)" /proc/version &>/dev/null; then
     is_wsl=true
 fi
 
-source "$(dirname "$BASH_SOURCE")/installer/docker.sh"
-source "$(dirname "$BASH_SOURCE")/installer/docker-compose.sh"
+#
+# docker installer
+#
+
+if [[ -z "$(which docker)" ]]; then
+    printf "You do not appear to have 'docker' installed (${HIGHLIGHT}https://docker.com${RESET})\n"
+
+    if $is_wsl; then
+        printf "We detected that you are running this installer under WSL and\n"
+        printf "that 'docker' is not installed. Please install Docker for Desktop\n"
+        printf "for the best experience: ${HIGHLIGHT}https://www.docker.com/products/docker-desktop${RESET}\n"
+        printf "\n"
+        printf "Do you want to continue installing? [y/N] "
+        read -n 1 -r
+        echo ""
+        if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+            exit 1
+        fi
+    else
+        printf "Since docker-blueprint is designed to work with docker, you will not be able to use it without docker\n"
+        printf "We can attempt to automatically install docker using convinience script:\n"
+        printf "${HIGHLIGHT}https://docs.docker.com/engine/install/ubuntu/#install-using-the-convenience-script${RESET}\n"
+        printf "\n"
+        printf "Do you want to automatically install docker? [Y/n] "
+        read -n 1 -r
+        echo ""
+        if [[ -z "$REPLY" ]] || [[ $REPLY =~ ^[Yy]$ ]]; then
+            echo "Trying to install docker..."
+            curl -fsSL https://get.docker.com | sh
+            if [[ $? > 0 ]]; then
+                printf "Unable to install docker, skipping..."
+            fi
+        fi
+    fi
+fi
+
+#
+# docker-compose installer
+#
+
+if [[ -z "$(which docker-compose)" ]]; then
+    printf "You do not appear to have 'docker-compose' installed (${HIGHLIGHT}https://docs.docker.com/compose/install${RESET})\n"
+
+    printf "Since docker-blueprint is designed to work with docker-compose, you will not be able to use it without docker-compose\n"
+    printf "We can attempt to automatically install docker-compose using curl:\n"
+    printf "${HIGHLIGHT}https://docs.docker.com/compose/install/#install-compose-on-linux-systems${RESET}\n"
+    printf "\n"
+    printf "Do you want to automatically install docker-compose? [Y/n] "
+    read -n 1 -r
+    echo ""
+    if [[ -z "$REPLY" ]] || [[ $REPLY =~ ^[Yy]$ ]]; then
+        echo "Trying to install docker-compose..."
+        sudo curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+        if [[ $? > 0 ]]; then
+            printf "Unable to install docker-compose, skipping..."
+        fi
+        sudo chmod +x /usr/local/bin/docker-compose
+    fi
+fi
 
 for PROGRAM in "${REQUIREMENTS[@]}"; do
     if [[ -z "$(which $PROGRAM)" ]]; then
@@ -25,7 +82,22 @@ for PROGRAM in "${REQUIREMENTS[@]}"; do
     fi
 done
 
-source "$(dirname "$BASH_SOURCE")/installer/yq.sh"
+#
+# yq installer
+#
+
+if [[ -z "$(which yq)" ]]; then
+    printf "You do not appear to have 'yq' installed (${HIGHLIGHT}https://github.com/mikefarah/yq${RESET})\n"
+    printf "For the best experience it is recommended to install a standalone version of 'yq'\n"
+    printf "\n"
+    printf "Do you want to attempt to automatically install 'yq'? [Y/n] "
+    read -n 1 -r
+    echo ""
+    if [[ -z "$REPLY" ]] || [[ $REPLY =~ ^[Yy]$ ]]; then
+        echo "Trying to install using webi..."
+        curl -sS https://webinstall.dev/yq@4 | bash
+    fi
+fi
 
 PROJECT_DIR=~/.docker-blueprint
 ENTRYPOINT="$PROJECT_DIR/entrypoint.sh"
