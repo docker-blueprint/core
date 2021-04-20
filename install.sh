5c8d945c3d826778e86929e71cb5c3c878cbb6b9
Aleksei Ivanov <contact@aleksei.dev>
2021-04-20 20:19:04 +0300

    Update installer
    Try to automatically install yq

--- install.sh
+++ install.sh
@@ -13,6 +13,19 @@ for PROGRAM in "${REQUIREMENTS[@]}"; do
     fi
 done
 
+if [[ -z "$(which yq)" ]]; then
+    printf "You do not appear to have 'yq' installed (https://github.com/mikefarah/yq)\n"
+    printf "For the best experience it is recommended to install a standalone version of 'yq'\n"
+    printf "\n"
+    printf "Do you want to attempt to automatically install 'yq'? [Y/n] "
+    read -n 1 -r
+    echo ""
+    if [[ -z "$REPLY" ]] || [[ $REPLY =~ ^[Yy]$ ]]; then
+        echo "Trying to install using webi..."
+        curl -sS https://webinstall.dev/yq@4 | bash
+    fi
+fi
+
 PROJECT_DIR=~/.docker-blueprint
 ENTRYPOINT="$PROJECT_DIR/entrypoint.sh"
 
