19c9f35c19c231ed86802b971f8a110c7d007a47
Aleksei Ivanov <contact@aleksei.dev>
2021-04-29 19:52:13 +0300

    Update pull.sh
    Silence output when ran as function

--- commands/pull.sh
+++ commands/pull.sh
@@ -61,17 +61,17 @@ while [[ "$#" -gt 0 ]]; do
 done
 
 if [[ -z "$BLUEPRINT" ]]; then
-    debug_print "No blueprint name provided - trying to resolve from $PROJECT_BLUEPRINT_FILE..."
+    ! $AS_FUNCTION && debug_print "No blueprint name provided - trying to resolve from $PROJECT_BLUEPRINT_FILE..."
 
     # Try to read blueprint name from docker-blueprint.yml
     if [[ -f "$PROJECT_BLUEPRINT_FILE" ]]; then
         yq_read_value BLUEPRINT 'from'
         if [[ -z "$BLUEPRINT" ]]; then
-            printf "${RED}ERROR${RESET}: Unable to resolve blueprint from project blueprint file.\n\n"
+            ! $AS_FUNCTION && printf "${RED}ERROR${RESET}: Unable to resolve blueprint from project blueprint file.\n\n"
             bash $ENTRYPOINT pull --help
             exit 1
         else
-            debug_print "Found blueprint '$BLUEPRINT' in project blueprint file"
+            ! $AS_FUNCTION && debug_print "Found blueprint '$BLUEPRINT' in project blueprint file"
         fi
     else
         bash $ENTRYPOINT pull --help
